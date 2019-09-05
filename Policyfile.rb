1dceb6622fc0aa1104632353c87a70ddb61d724d
Mandi Walls <mandi.walls@gmail.com>
2019-09-05 11:18:27 -0400

    pushing to github

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,7 +10,7 @@ name 'base'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'base::default'
+run_list 'os-hardening::default','base::default'
 
 # Specify a custom source for a single cookbook:
 cookbook 'base', path: '.'
